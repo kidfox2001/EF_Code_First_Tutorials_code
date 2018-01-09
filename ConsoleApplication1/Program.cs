4ea3533e19080b5494adf841a12b7e9b5101ee45
kidfox2001 <kidfox2001@yahoo.com>
2018-01-09 16:09:50 +0700

    1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_Code_First_Tutorials;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // เปิดดูที่ objct sql server
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                using (var ctx = new SchoolContext())
+                {
+                    Student stud = new Student() { StudentName = "New Student2" };
+
+                    ctx.Students.Add(stud);
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("finnish");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
